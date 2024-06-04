1f0a84189d6d4f68821cb15580669e42f119dd76
Thiago <168425342+Thiagofrancaveloso@users.noreply.github.com>
2024-06-04 09:02:21 -0300

    Create ClasseCompostaFuncionario.java

---
+++ ClasseCompostaFuncionario.java
@@ -0,0 +1,39 @@
+package AulaBia;
+
+public class Funcionario {
+
+	private String nome;
+	private String CPF;
+	private Data data;
+	private Endereco endereco;
+	
+	public Funcionario(String novoNome, String novoCPF, Data novaData, Endereco novoEndereco) {
+		this.nome = novoNome;
+		this.CPF = novoCPF;
+		this.data = novaData;
+		this.endereco = novoEndereco;
+		}
+	
+	public String getNome () {
+		return nome;
+	}
+	
+	public String getCPF() {
+		return CPF;
+	}
+	
+	public void setNome (String novoNome) {
+		this.nome = novoNome;
+	}
+	
+	public void setDataNasc (Data novaData) {
+		this.data = novaData;
+	}
+	
+	public void setEndereco (Endereco novoEndereco) {
+		this.endereco = novoEndereco;
+	}
+	public String toString() {
+		return "Nome: " + this.nome + "\nCPF: " + this.CPF + "\nData Nascimento: " + this.data + "\nEndereço: " + this.endereco;
+	}
+}
